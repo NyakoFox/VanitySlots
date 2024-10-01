43e79c31a3edbe72bf968f15dbfbca6708b6a0a5
NyakoFox <nyakowofox@gmail.com>
2024-10-01 11:41:18 -0300

    Add familiar armor to creative inventory

--- fabric/src/main/java/gay/nyako/vanityslots/VanitySlotsClientMod.java
+++ fabric/src/main/java/gay/nyako/vanityslots/VanitySlotsClientMod.java
@@ -2,11 +2,26 @@ package gay.nyako.vanityslots;
 
 import net.fabricmc.api.ClientModInitializer;
 import fuzs.forgeconfigapiport.fabric.api.neoforge.v4.client.ConfigScreenFactoryRegistry;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.neoforged.neoforge.client.gui.ConfigurationScreen;
 
 public class VanitySlotsClientMod implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         ConfigScreenFactoryRegistry.INSTANCE.register(Constants.MOD_ID, ConfigurationScreen::new);
+
+        registerItemGroups();
+    }
+
+    private void registerItemGroups()
+    {
+        ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.COMBAT)
+                .register((itemGroup) -> {
+                    itemGroup.accept(VanitySlotsItems.FAMILIAR_WIG);
+                    itemGroup.accept(VanitySlotsItems.FAMILIAR_SHIRT);
+                    itemGroup.accept(VanitySlotsItems.FAMILIAR_PANTS);
+                    itemGroup.accept(VanitySlotsItems.FAMILIAR_SNEAKERS);
+                });
     }
 }
--- neoforge/src/main/java/gay/nyako/vanityslots/VanitySlotsClientMod.java
+++ neoforge/src/main/java/gay/nyako/vanityslots/VanitySlotsClientMod.java
@@ -1,15 +1,29 @@
 package gay.nyako.vanityslots;
 
+import net.minecraft.world.item.CreativeModeTabs;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.client.gui.ConfigurationScreen;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
+import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
 
 @Mod(value = Constants.MOD_ID, dist = Dist.CLIENT)
 public class VanitySlotsClientMod {
     public VanitySlotsClientMod(ModContainer container)
     {
         container.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
+
+        var eventBus = container.getEventBus();
+        eventBus.addListener(this::buildContents);
+    }
+
+    public void buildContents(BuildCreativeModeTabContentsEvent event) {
+        if (event.getTabKey() == CreativeModeTabs.COMBAT) {
+            event.accept(VanitySlotsItems.FAMILIAR_WIG);
+            event.accept(VanitySlotsItems.FAMILIAR_SHIRT);
+            event.accept(VanitySlotsItems.FAMILIAR_PANTS);
+            event.accept(VanitySlotsItems.FAMILIAR_SNEAKERS);
+        }
     }
 }
